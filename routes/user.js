c029327082d7d0d3386546cc7079ee885512746c
Lokendra Dewangan <lokendra.intern@globussoft.in>
2023-02-22 15:56:58 +0530

    "added login api"

--- routes/user.js
+++ routes/user.js
@@ -1,43 +1,43 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const {User, singupValidate, loginValidate} = require('../models/User.js');
+const { User, signupValidate, loginValidate } = require('../models/User.js');
 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
-        const {error} = validate(req.body);
-        if(error)
-            return res.status(400).send({message: error.details[0].message});
-        
-        const user = await User.findOne({email: req.body.email});
-        if(user)
-            return res.status(409).send({message: 'User with given email already exists'});
-        
+        const { error } = signupValidate(req.body);
+        if (error) return res.status(400).send({ message: error.details[0].message });
+
+        const user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(409).send({ message: 'User with given email already exists' });
+
         const salt = await bcrypt.genSalt(Number(10));
         const hashPassword = await bcrypt.hash(req.body.password, salt);
-        await new User({...req.body, password: hashPassword}).save();
-        return res.status(201).send({message: 'User created successfully'});
+        await new User({ ...req.body, password: hashPassword }).save();
+        return res.status(201).send({ message: 'User created successfully' });
     } catch (error) {
         console.log(error);
-        res.status(500).send({message: 'Ineternal server error'})
+        res.status(500).send({ message: 'Internal server error' });
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
-        const {error} = validate(req.body);
-        if(error)
-            return res.status(400).send({message: error.details[0].message});
-        
-        const user = await User.findOne({email: req.body.email});
-        if(user)
-            return res.status(409).send({message: 'User with given email already exists'});
-        
-        const salt = await bcrypt.genSalt(Number(10));
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-        await new User({...req.body, password: hashPassword}).save();
-        return res.status(201).send({message: 'User created successfully'});
+        const { error } = loginValidate(req.body);
+        if (error) return res.status(400).send({ message: error.details[0].message });
+
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(401).send({ message: 'Invalid Email or Password' });
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) return res.status(401).send({ message: 'Invalid Email or Password' });
+        const token = user.generateAuthToken();
+        res.status(200).send({ data: token, message: 'Logged in succesfully' });
+        await new User({ ...req.body, password: hashPassword }).save();
+        return res.status(201).send({ message: 'User created successfully' });
     } catch (error) {
         console.log(error);
-        res.status(500).send({message: 'Ineternal server error'})
+        res.status(500).send({ message: 'Internal server error' });
     }
 });
+
+module.exports = router;
